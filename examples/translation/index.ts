a99a708076e4415502b746730a8749a4b0f138b3
OnCloud <anonymousaaaa41414141@gmail.com>
2025-08-02 00:39:43 +0800

    fix(examples): update translateJson function call to use target language

--- examples/translation/index.ts
+++ examples/translation/index.ts
@@ -1,6 +1,7 @@
 import { sampleData } from "modules/sample-data";
 import { translateJson } from "modules/translate-json";
 import { RedisClient } from "modules/redis";
+import { TargetLanguage } from "modules/translate-json/modules/translate-batch";
 
 (async () => {
   await new RedisClient().init(process.env.REDIS_URL);
@@ -8,6 +9,7 @@ import { RedisClient } from "modules/redis";
   const translatedJson = await translateJson({
     jsonData: sampleData.a,
     batchSize: 10,
+    targetLanguage: TargetLanguage.ZH_TW,
   });
 
   console.log(JSON.stringify(translatedJson, null, 2));
