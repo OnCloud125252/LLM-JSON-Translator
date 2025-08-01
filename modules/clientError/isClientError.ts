687d4bd043097b2058d8184a535784991c24500c
OnCloud <anonymousaaaa41414141@gmail.com>
2025-08-02 03:00:04 +0800

    refactor(clientError): add type safety check for error object validation

--- modules/clientError/isClientError.ts
+++ modules/clientError/isClientError.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from "http-status-codes";
 
 export function isClientError(error: any) {
   return (
+    typeof error === "object" &&
     "__type__" in error &&
     error.__type__ === "CLIENT_ERROR" &&
     "code" in error &&
